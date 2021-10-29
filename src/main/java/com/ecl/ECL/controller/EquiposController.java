7c385c7e82cbb28a4d2c8ebdfe7732eed43c3562
Michael Hamilton Sanchez <michael.hamilton@qaracter.es>
2021-10-29 16:10:42 +0200

    Creación de EquiposController (crear, modificar y eliminar)

---
+++ src/main/java/com/ecl/ECL/controller/EquiposController.java
@@ -0,0 +1,63 @@
+package com.ecl.ECL.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import com.ecl.ECL.model.Equipos;
+import com.ecl.ECL.repository.EquiposRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class EquiposController {
+
+    private final EquiposRepository repository;
+
+    public EquiposController(EquiposRepository repository) {
+        this.repository = repository;
+    }
+
+    //Agregate root
+    //tag::get-aggregate-root[]
+    @GetMapping("/equipos")
+    List<Equipos> all(){
+        return repository.findAll();
+    }
+    //end::get-aggregate-root[]
+
+    @PostMapping("/equipos")
+    Equipos newEquipos(@RequestBody Equipos newEquipos){
+        return repository.save(newEquipos);
+    }
+
+    //Single item
+
+    @GetMapping("/equipos/{id}")
+    Equipos one(@PathVariable Long id){
+        return repository.findById(id)
+                .orElseThrow(()->new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+    @PutMapping("/equipos/{id}")
+    Equipos replaceEquipos(@RequestBody Equipos newEquipos, @PathVariable Long id){
+        return repository.findById(id)
+                .map(equipos -> {
+                    equipos.setNombre(newEquipos.getNombre());
+                    equipos.setPais(newEquipos.getPais());
+                    equipos.setEquipoRivalHistoricamente(newEquipos.getEquipoRivalHistoricamente());
+                    equipos.setEscudo(newEquipos.getEscudo());
+                    return repository.save(equipos);
+                })
+                .orElseGet(()->{
+                    newEquipos.setId(id);
+                    return repository.save(newEquipos);
+                });
+
+    }
+
+    @DeleteMapping("/equipos/{id}")
+    void deleteEquipos(@PathVariable Long id){
+        repository.deleteById(id);
+    }
+}
